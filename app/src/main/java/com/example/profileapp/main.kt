7683981a649ec4ea3716aac0ec7cfb4998b7c1db
Ogene Joseph <jsg4704a@etu.iut-tlse3.fr>
2024-12-03 15:36:06 +0100

    exercice2 ok

--- app/src/main/java/com/example/profileapp/main.kt
+++ app/src/main/java/com/example/profileapp/main.kt
@@ -34,8 +34,27 @@ val piece= listOf(Cuisine("cuisine ",2F,20F),Salon("salon",20F,20F))
     }
 
    //exercice2
-   class Etudiant(name:String,promo:String,matière:String)
+   class Etudiant(val name:String,val promo:String,val matieres:List<String>)
 
+    val etudiant= listOf(
+        Etudiant("Paul","2025", listOf("mobile","web","bdd")),
+        Etudiant("Yazid","2024",listOf("mobile","android","network")),
+        Etudiant("Caroline","2025",listOf("SE","Anglais"))
+    )
+
+
+
+    println("taille du tableau=${etudiant.size} elements")
+    val promo2024= etudiant.filter{ it.promo=="2024"}
+
+    promo2024.forEach{println("etudiant de la promo 2024 nom=${it.name}")}
+
+
+    val etu2matiere= etudiant.filter { it.matieres.size>2 }
+    etu2matiere.forEach{println("etudiant suivant plus de 2 matières nom=${it.name}")}
+
+    val sommeMatiere= etudiant.fold(0){acc,e->acc+e.matieres.size}
+println("le nombre de matière est $sommeMatiere")
 
 
 
